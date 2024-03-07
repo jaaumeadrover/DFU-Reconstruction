de0e3ed68941887c1e3ce755911db9484c25ecf7
Jaume Adrover Fernández <jaumeadroverfernandez@gmail.com>
2024-03-07 18:31:13 +0100

    frame extraction

--- utils/path.py
+++ utils/path.py
@@ -1,13 +1,24 @@
+"""
+
+"""
 import os
 
 """
 Given a patient_date path, extract bag,color,depth relative locations.
 """
-
-
 def getAllPaths(path):
     paths = {'bag': os.path.join(path, 'bag'),
              'color': os.path.join(path, 'color'),
              'depth': os.path.join(path, 'depth')
              }
     return paths
+
+def getImageInfo(src):
+    parts  = src.split('_')
+    num_bag = parts[3].split('.')[0]
+    return {'patient': parts[0]+parts[1], 'date': parts[2], 'numBag': num_bag}
+def createColDepthImgNames(src, frame):
+    base_name = '_'.join(src.values())
+    color = base_name +'_c_' + frame
+    depth = base_name +'_d_' + frame
+    return {'color': color, 'depth': depth}
