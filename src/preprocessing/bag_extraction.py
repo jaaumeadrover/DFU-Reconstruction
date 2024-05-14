173fdda8a66c7d913a603e8ee41b8433c6ed47ce
Jaume Adrover Fernández <jaumeadroverfernandez@gmail.com>
2024-05-14 19:00:27 +0200

    bag_extraction cleaned

--- src/preprocessing/bag_extraction.py
+++ src/preprocessing/bag_extraction.py
@@ -1,31 +1,25 @@
-import os
-
-import cv2
-from tqdm import tqdm
-from utils.create_dataset import imagesFromFile
-from utils.path import getAllPaths, getVideoInfo, createImageFolders
-from definitions import ROOT_DIR
 """
-TITLE: bag_extraction
-DATE: 05/03/2024
+TITLE: bag_extraction.py
 AUTHOR: Jaume Adrover Fernández
 DESCRIPTION: script in which we'll basically
 preprocess all our dataset, extracting color and depth frames from
 .bag files.
+CREATION DATE: 05/03/2024
 """
+import os
 
-data_path = ROOT_DIR + '/data/'
+from tqdm import tqdm
+from definitions import DATA_DIR
+from utils.create_dataset import imagesFromFile
+from utils.path import getAllPaths, getVideoInfo, createSomeFolders, getPatientsFolders
 
-for patient in tqdm(os.listdir(data_path), desc='Converting all videos to frames'):
-    patient_path = os.path.join(data_path, patient)
+# Main loop
+for patient in tqdm(getPatientsFolders(), desc='Converting all videos to frames'):
+    patient_path = os.path.join(DATA_DIR, patient)  # Get Patient Date
     for date in os.listdir(patient_path):
-         paths = getAllPaths(os.path.join(patient_path, date))
-         createImageFolders(list(paths.values())[1:])
-         for filename in os.listdir(paths['bag']):
-             video_path = os.path.join(paths['bag'], filename)   # Get img path
-             video_info = getVideoInfo(filename)                 # Get img info
-             imagesFromFile(video_path, paths, video_info)       # Get imgs from video
-
-
-
-
+        paths = getAllPaths(os.path.join(patient_path, date))
+        createSomeFolders(list(paths.values())[1:])  # Create color/depth img folders
+        for filename in os.listdir(paths['bag']):
+            video_path = os.path.join(paths['bag'], filename)  # Get .bag path
+            video_info = getVideoInfo(filename)  # Get video info
+            imagesFromFile(video_path, paths, video_info)  # Get imgs from recorded sequence
